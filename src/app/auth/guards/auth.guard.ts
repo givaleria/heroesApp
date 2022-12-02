6999a6d92b3d1e2b11799cdfac9e3bfe1fc2f8f4
gisela degiglio <giselavaleria19@gmail.com>
2022-12-02 20:18:26 -0300

    end of section 15

---
+++ src/app/auth/guards/auth.guard.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment } from '@angular/router';
+import { Observable, tap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate, CanLoad {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+
+      return this.authService.verifyAuth()
+                 .pipe(
+                  tap( isAuthenticated => {
+                    if (!isAuthenticated) {
+                      this.router.navigate(['./auth/login'])
+                    }
+                  })
+                 )
+
+    //   if (this.authService.auth.id) {
+    //     return true;
+    //   }
+
+    // console.log('Block by AuthGuard - canActivate')
+    // return false;
+  }
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+
+      return this.authService.verifyAuth()
+                 .pipe(
+                   tap( isAuthenticated => {
+                     if (!isAuthenticated) {
+                       this.router.navigate(['./auth/login'])
+                     }
+                   })
+                 )
+
+    //   if (this.authService.auth.id) {
+    //     return true;
+    //   }
+
+    // console.log('Block by AuthGuard - canLoad')
+    // return false;
+  }
+}
